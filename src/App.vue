bf124f40ced9337e87cd35dea4d53698c74a080f
Mukeshbindapi <112742726+Mukeshbindapi@users.noreply.github.com>
2024-04-19 11:33:53 +0530

    init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Home></Home>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Home
+  }
+}
+</script>
+
+<style>
+  #app {
+    text-align: center;
+  }
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>{{text}}</h1>
+        <div v-html="tag"></div>
+        <div v-html="tag2"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomE',
+    data(){
+        return {
+            text : "Mukesh Bindra ji",
+            tag : '<h1>Mukesh Kumar</h1>',
+            tag2 : '<h2>mukesh@test.com</h2>'
+        }
+    }
+};
+</script>
+
+<style scoped>
+    h1 {
+        color: orange;
+    }
+</style>
