880857bb39f94958fb3bac80b3973807296fc440
Mukeshbindapi <112742726+Mukeshbindapi@users.noreply.github.com>
2024-04-19 11:33:53 +0530

    init

---
+++ src/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>Get input filed value</h1>
+        <input type="text" placeholder="enter email" v-model="email"><br><br>
+        <input type="text" placeholder="enter password" v-model="password"><br><br>
+        <button v-on:click="getData()" type="button">Get Value</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomE',
+    data(){
+        return {
+            email : null,
+            password : null
+        }
+    },
+    methods : {
+        getData(){
+            console.warn("values :",this.email,this.password);
+        }
+    }
+};
+</script>
+
+<style scoped>
+h1 {
+    color: orange;
+}
+</style>
