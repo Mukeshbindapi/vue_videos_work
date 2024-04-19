9d9735f2d94868fcd90a9dd19db76eec6d2138c3
Mukeshbindapi <112742726+Mukeshbindapi@users.noreply.github.com>
2024-04-19 11:33:53 +0530

    init

---
+++ src/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h1 v-on:mousemove="getConsole()">Home Component</h1>
+        <button v-on:click="getData()">Click me</button>
+        <button v-on:dblclick="getData('button double clicked')">Click me</button>
+        <div>{{count}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomE',
+    data(){
+        return {
+            count : 0
+        }
+    },
+    methods:{
+        getData(data)
+        {
+            this.count = this.count+1;
+        },
+        getConsole(){
+            console.warn("function colled");
+        }
+    }
+};
+</script>
+
+<style scoped>
+h1 {
+    color: orange;
+}
+</style>
