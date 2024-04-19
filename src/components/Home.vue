2a2d0327087ce566d2c6a404728e42e728c5b661
Mukeshbindapi <112742726+Mukeshbindapi@users.noreply.github.com>
2024-04-19 11:33:53 +0530

    init

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>Two Way Binding</h1>
+        <input type="text" v-model="count" />
+        <h2>{{count}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomE',
+    data(){
+        return {
+            count : 10
+        }
+    }
+};
+</script>
+
+<style scoped>
+h1 {
+    color: orange;
+}
+</style>
