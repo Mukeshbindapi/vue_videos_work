76dcbc05f278db3476d7221fdfa2f737176a5340
Mukeshbindapi <112742726+Mukeshbindapi@users.noreply.github.com>
2024-04-19 11:33:53 +0530

    init

---
+++ src/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>Ref in vue js 3</h1>
+        <input type="text" ref="first_input">
+        <button @click="getData">Click me</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomE',
+    methods : {
+        getData() {
+            this.$refs.first_input.focus();
+            let val = this.$refs.first_input.value;
+            console.warn(val);
+            this.$refs.first_input.style.color = 'red'
+        }
+    }
+};
+</script>
+
+<style scoped>
+h1 {
+    color: orange;
+}
+</style>
