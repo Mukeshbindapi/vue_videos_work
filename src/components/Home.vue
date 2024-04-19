0966789c0024c52e22edacaedae4d22f783dee2c
Mukeshbindapi <112742726+Mukeshbindapi@users.noreply.github.com>
2024-04-19 11:33:53 +0530

    init

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1 v-if="show">if condtion</h1>
+        <h1 v-else>else condtion</h1>
+        <button v-on:click="show=!show">toogle</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomE',
+    data(){
+        return {
+           show : false
+        }
+    }
+};
+</script>
+
+<style scoped>
+h1 {
+    color: orange;
+}
+</style>
