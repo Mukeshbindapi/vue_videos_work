aeb9d09b03a806a55284d68d07ace07a53c3b0ef
Mukeshbindapi <112742726+Mukeshbindapi@users.noreply.github.com>
2024-04-19 11:33:53 +0530

    init

---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1>Email : {{email}}</h1>
+    <h1>Mobile : {{mobile}}</h1>
+    <h1>Name : {{getName('mukesh')}}</h1>
+</template>
+
+<script>
+    export default {
+        name : 'HomE',
+        data(){
+            return {
+                email : 'mukesh@gmail.com',
+                mobile : '2345617895',
+                getName:function(a){
+                    return a;
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    h1{
+        color: orange;
+    }
+</style>
