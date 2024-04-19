d23303ef4f2f8e35781d2520f450dfceb60fddca
Mukeshbindapi <112742726+Mukeshbindapi@users.noreply.github.com>
2024-04-19 11:33:53 +0530

    init

---
+++ src/components/Home.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <h1>for loop in vue js </h1>
+        <ul>
+            <li v-for="item in technology" :key="item">{{ item }}</li>
+        </ul>
+        <br><br>
+        <ul>
+            <li v-for="item in user" :key="item.email">
+                Name is : {{ item.name }} and email is : {{ item.email }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomE',
+    data(){
+        return {
+            technology : ['java','php','node','html'],
+            user : 
+            [
+                {
+                    name : 'mukesh kumar',
+                    email : 'mukesh@gmail.com'
+                },
+                {
+                    name : 'ashish kumar',
+                    email : 'ashish@gmail.com'
+                },
+                {
+                    name : 'jasavant kumar',
+                    email : 'jasavant@gmail.com'
+                }
+            ]
+        }
+    }
+};
+</script>
+
+<style scoped>
+h1 {
+    color: orange;
+}
+</style>
