0e6da59cd1aff235f5cbaa90abe4e5855a1fdc7e
Mukeshbindapi <112742726+Mukeshbindapi@users.noreply.github.com>
2024-04-19 11:33:53 +0530

    init

---
+++ src/components/Home.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <h1>Reuse Component</h1>
+        <ul>
+            <li v-for="item in users" :key="item.name">
+                <User :data="item" :getData="getData" />
+            <</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import User from './User.vue';
+export default {
+    name: 'HomE',
+    components : {
+        User
+    },
+    methods:{
+        getData(name,email){
+            alert(name,email)
+        }
+    },
+    data(){
+        return {
+            users : [
+                {
+                    name : 'Mukesh Kumar',
+                    email : 'mukesh123@gmail.com'
+                },
+                {
+                    name : 'Ashish Kumar',
+                    email : 'ashish123@gmail.com'
+                },
+                {
+                    name : 'Jasavant Kumar',
+                    email : 'jasavant123@gmail.com'
+                },
+                {
+                    name : 'Himanshu Kumar',
+                    email : 'himanshu123@gmail.com'
+                },
+                {
+                    name : 'Jaihind Kumar',
+                    email : 'jaihind123@gmail.com'
+                }
+            ]
+        }
+    }
+};
+</script>
+
+<style scoped>
+    h1 {
+        color: orange;
+    }
+</style>
---
+++ src/components/User.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="user">
+        <h1>{{data.name}}</h1>
+        <h2>{{ data.email }}</h2>
+        <button v-on:click="getData(data.name)">Alert Name</button>
+        <button v-on:click="getData(data.email)">Alert Email</button>
+    </div>
+   
+</template>
+
+<script>
+    export default {
+        name : 'UseR',
+        props:{
+            data : Object,
+            getData : Function
+        }
+    }
+</script>
+
+<style>
+.user{
+    background-color: skyblue;
+    padding: 10px;
+    border-bottom: 1px solid;
+    margin-bottom: 20px;
+
+}
+</style>
