81159a636c8042a9a1e4b0fd0b1a1cd330eda5b0
Mukeshbindapi <112742726+Mukeshbindapi@users.noreply.github.com>
2024-04-19 11:33:53 +0530

    init

---
+++ src/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1>senr data child to parent in vue js 3</h1>
+    </div>
+    <h1>{{ childUser }}</h1>
+    <User :getUser="getUserName" />
+</template>
+
+<script>
+import User from './User.vue'
+export default {
+    name: 'HomE',
+    components : {
+        User
+    },
+    data(){
+        return {
+            childUser : ''
+        }
+    },
+    methods : {
+        getUserName(name){
+            this.childUser = name
+        }
+    }
+};
+</script>
+
+<style scoped>
+h1 {
+    color: orange;
+}
+</style>
---
+++ src/components/User.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>User component</h1>
+    <button @click="getUser(UserName)">Send User Name</button>
+</template>
+
+<script>
+    export default {
+        name : 'UseR',
+        data(){
+            return {
+                UserName : "Mukesh Kumar"
+            }
+        },
+        props:{
+            getUser : Function
+        }
+    }
+</script>
