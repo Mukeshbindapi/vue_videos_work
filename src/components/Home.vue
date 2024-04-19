e8d1778342f623e64ac25462419b6b5dc117d035
Mukeshbindapi <112742726+Mukeshbindapi@users.noreply.github.com>
2024-04-19 11:33:53 +0530

    init

---
+++ src/components/Home.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h1>Pass data to Child Component</h1>
+    </div>
+    <Child name="Mukesh Kumar" :user="user" :getData="getData" />
+</template>
+
+<script>
+import Child from './Child.vue'
+export default {
+    name: 'HomE',
+    components : {
+        Child
+    },
+    data()
+    {
+        return {
+            user : {name : 'mukesh kumar', email : 'mukeshbindra@gmail.com'}
+        }
+    },
+    methods : {
+        getData(){
+            console.warn("function colled");
+            alert("Hellow")
+        }
+    }
+};
+</script>
+
+<style scoped>
+    h1 {
+        color: orange;
+    }
+</style>
