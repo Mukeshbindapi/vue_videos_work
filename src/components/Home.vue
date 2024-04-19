4c0b13975c9a212d9fbea18ca43cd39e2ba09f54
Mukeshbindapi <112742726+Mukeshbindapi@users.noreply.github.com>
2024-04-19 11:33:53 +0530

    init

---
+++ src/components/Home.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h1>Email: {{ email }}</h1>
+        <h1>Name: {{ getName('Mukesh') }}</h1>
+        <h1>All Data: {{ getData().email }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    data() {
+        return {
+            email: 'mukesh@gmail.com',
+        };
+    },
+    methods: {
+        getName(name) {
+            return name;
+        },
+        getData() {
+            return {
+                name: 'Mukesh Kumar',
+                email: this.email,
+            };
+        },
+    },
+};
+</script>
+
+<style scoped>
+h1 {
+    color: orange;
+}
+</style>
