362efedd8ed4c0390ad90311760abe294fb1c50b
Mukeshbindapi <112742726+Mukeshbindapi@users.noreply.github.com>
2024-04-19 11:33:53 +0530

    init

---
+++ src/components/Home.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <h1>CSS Binding</h1>
+        <h2 :class="ApplyStyle">Home Component</h2>
+        <button @click="colorfull = !colorfull">Apply Style</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    data() {
+        return {
+           colorfull: true,
+        }
+    },
+    computed: {
+        ApplyStyle() {
+            return {
+                test: this.colorfull,
+                test1: true,
+                other: true
+            }
+        }
+    }
+};
+</script>
+
+<style scoped>
+h1 {
+    color: orange;
+}
+
+.test {
+    background-color: green;
+    width: 200px;
+    padding: 10px;
+    text-align: center;
+    margin-left: 42%;
+    color: #fff;
+}
+
+.test1 {
+    color: red;
+    width: 200px;
+    padding: 10px;
+    text-align: center;
+    margin-left: 42%;
+}
+
+.other {
+    font-size: 50px;
+}
+</style>
